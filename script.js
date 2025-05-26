37205a27a5fc039132553976a092c636aaa38f8e
jovanao1 <jocaa.obrenic@gmail.com>
2025-05-26 19:08:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var dugme = document.getElementById("btn");
+
+dugme.addEventListener('click', function(){
+    let blok = document.getElementsByClassName("share-popup")[0];
+    blok.style.display = 'block';
+});
